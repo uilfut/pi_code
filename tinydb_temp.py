3b9838bbb6a5f019a68d9ebbad7f3d20be0a3528
uilfut <willfoot@gmail.com>
2018-03-20 13:07:28 -0400

    working from crontab to tinydb

--- tinydb_temp.py
+++ tinydb_temp.py
@@ -1,13 +1,17 @@
 from tinydb import TinyDB, Query
 import time
 from w1thermsensor import W1ThermSensor
-
+from datetime import datetime
+import json
 db = TinyDB('tinydb_environment.json')
 
 sensor = W1ThermSensor()
-
 while True:
     tempC = float(sensor.get_temperature(W1ThermSensor.DEGREES_C))
-    db.insert({'date_time':date_time, 'tempC': tempC})
-    time.sleep(60)
+    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    db.insert({'date_time': timestamp, 'tempC': tempC})
+    time.sleep(10)
 
+#    time_dict = {'datetime': datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}
+#    time_dict = {datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}
+#    db.insert(time_dict, 'tempC': tempC})
